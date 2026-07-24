d03fda282dfb697b59174a9b5282b647a18974e0
harinisharma-t <harinisharma.t@gmail.com>
2026-07-24 22:03:08 +0530

    Day 11: Improve UI and clean up application

--- app/app.py
+++ app/app.py
@@ -31,8 +31,8 @@ def predict():
     ]
 
     prediction = model.predict([features])
+    
     probability = model.predict_proba([features])
-
     confidence = round(max(probability[0]) * 100, 2)
 
     if prediction[0] == 1:
@@ -43,11 +43,11 @@ def predict():
         color = "#198754"
 
     return render_template(
-    "result.html",
-    prediction=result,
-    color=color,
-    confidence=confidence
-)
+        "result.html",
+        prediction=result,
+        color=color,
+        confidence=confidence
+    )
 
 
 if __name__ == "__main__":
