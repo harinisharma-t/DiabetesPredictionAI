698ffaf4e29d43e49de64c279cfb91c11c83fa2c
harinisharma-t <harinisharma.t@gmail.com>
2026-07-27 22:13:33 +0530

    Day 14: Add About page and navigation bar

--- app/app.py
+++ app/app.py
@@ -74,5 +74,9 @@ def history():
         predictions=predictions
     )
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
